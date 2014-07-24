39eec75cd651b16d4be8ff4d224a3a6f94e8a302
Chris Saylor <cjsaylor@gmail.com>
2014-07-24 17:19:55 -0400

    Added container collection.

---
+++ src/Container/ContainerCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Boxmeup\Container;
+
+use Cjsaylor\Domain\Collection;
+
+class ContainerCollection extends Collection {
+
+	public function get($id) {
+		$items = $this->getItems();
+		return isset($items[$id]) ? $items[$id] : null;
+	}
+
+	public function add(Container $container) {
+		$this->getItems()[$container['id']] = $container; 
+	}
+
+}
---
+++ tests/Container/ContainerCollectionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Boxmeup\Test\Container;
+
+use Boxmeup\Container\Container;
+use Boxmeup\Container\ContainerCollection;
+
+class ContainerCollectionTest extends \PHPUnit_Framework_TestCase {
+
+	public function testAddAndGet() {
+		$container = new Container(['id' => '1', 'name' => 'test container']);
+		$collection = new ContainerCollection();
+		$collection->add($container);
+		$this->assertCount(1, $collection);
+		$this->assertInstanceOf('\Boxmeup\Container\Container', $collection->get('1'));
+	}
+
+}
