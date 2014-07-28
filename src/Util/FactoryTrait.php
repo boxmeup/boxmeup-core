d92e12d96f313af88efed2cf3b516e1d85b2a465
Chris Saylor <cjsaylor@gmail.com>
2014-07-28 19:38:21 -0400

    Created a simple factory trait.

---
+++ src/Util/FactoryTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Boxmeup\Util;
+
+trait FactoryTrait
+{
+	/**
+	 * Factory method for statically intantiating a class.
+	 *
+	 * @return mixed
+	 */
+	public static function factory() {
+		return new static();
+	}
+}
