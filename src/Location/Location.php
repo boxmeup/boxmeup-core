cd9f2388e6a3052a298be9309ac8c4ee0305d242
Chris Saylor <cjsaylor@gmail.com>
2014-07-28 19:39:21 -0400

    Added very basic structure for location entity.

--- src/Location/Location.php
+++ src/Location/Location.php
@@ -5,3 +5,19 @@ namespace Boxmeup\Location;
 use \Cjsaylor\Domain\Entity,
 	\Boxmeup\Schema\SchemaValidatable,
 	\Boxmeup\Schema\DefaultEntitySchemaValidate;
+
+class Location extends Entity implements SchemaValidatable
+{
+	use DefaultEntitySchemaValidate;
+
+	/**
+	 * Schema for containers items.
+	 *
+	 * @return array
+	 */
+	public function getRequiredSchema() {
+		return [
+			'name'
+		];
+	}
+}
