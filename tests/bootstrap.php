1a35e6f23f54536e5614cb6bc1b92fa00294f426
Chris Saylor <cjsaylor@gmail.com>
2014-07-28 23:45:35 -0400

    Created new bootstrap for db connection setup.

---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Boxmeup\Test
+{
+	include __DIR__ . '/../vendor/autoload.php';
+
+	function getConnection() {
+		static $conn = null;
+
+		if (!$conn) {
+			$conn = \Doctrine\DBAL\DriverManager::getConnection([
+				'dbname' => $GLOBALS['name'],
+				'user' => $GLOBALS['user'],
+				'password' => $GLOBALS['password'],
+				'host' => $GLOBALS['host'],
+				'driver' => 'pdo_mysql'
+			]);
+		}
+
+		return $conn;
+	}
+}
