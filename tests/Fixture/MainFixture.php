119560bb1aa2794acd886aca56cc7ba149bc6a63
Chris Saylor <cjsaylor@gmail.com>
2014-07-28 23:46:46 -0400

    Implemented main test data fixture.

---
+++ tests/Fixture/MainFixture.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Boxmeup\Test\Fixture;
+
+use Boxmeup\Util\FactoryTrait;
+use Boxmeup\Test\FixtureInterface;
+
+class MainFixture implements FixtureInterface
+{
+	use FactoryTrait;
+
+	public function getRecords() {
+		return [
+			'users' => [
+				[
+					'id' => 1,
+					'email' => 'test@test.com',
+					'password' => 'somehash',
+					'uuid' => 'deprecated',
+					'created' => '2014-01-01 00:00:00',
+					'modified' => '2014-01-01 00:00:00'
+				]
+			],
+			'containers' => [
+				[
+					'id' => 1,
+					'user_id' => 1,
+					'uuid' => 'deprecated',
+					'name' => 'Box 1',
+					'slug' => 'box-1',
+					'container_item_count' => 1,
+					'created' => '2014-01-01 00:00:00',
+					'modified' => '2014-01-01 00:00:00'
+				]
+			]
+		];
+	}
+	
+}
---
+++ tests/FixtureInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Boxmeup\Test;
+
+interface FixtureInterface
+{
+	/**
+	 * Get an array of records to insert as part of the test case bootstrap.
+	 *
+	 * @return array
+	 */
+	public function getRecords();
+}
